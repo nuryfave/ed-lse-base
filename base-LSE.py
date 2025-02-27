ffa66e9cf8fef214193e77635d85b594fe077ca3
Ing. Nury Farelo Velásquez <129210791+nuryfave@users.noreply.github.com>
2025-02-27 10:56:47 -0500

    Base de repositorio

---
+++ base-LSE.py
@@ -0,0 +1,32 @@
+# ===========================================
+# By: Nury Farelo - Estructuras Datos
+# Name: Lista Simplemente Enlazada 
+# ===========================================
+
+# Clase Nodo
+class Nodo:
+	def __init__(self, data):
+		self.data = data
+		self.siguiente = None
+
+# CLase Listas enlazada simple
+class ListaSE:
+	def __init__(self):
+		self.cabeza = None
+  
+  	# Lista Vacia
+	def vacio(self):
+		if self.cabeza == None:
+			print("Está vacia")
+		else:
+			print("Lista no vacia")
+
+	# Agregar al inicio
+	def agregarInicio(self, data):
+		nuevo_nodo = Nodo(data)
+		if self.cabeza is None:
+			self.cabeza = nuevo_nodo
+			return
+		else:
+			nuevo_nodo.siguiente = self.cabeza
+			self.cabeza = nuevo_nodo
